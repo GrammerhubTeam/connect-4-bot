6716087e9c1cb5b31c8786fe6e2e5dbc16369c7c
Kyle Williams <kwilliams@peakactivity.com>
2020-07-08 00:12:36 -0400

    July 8 - CONNECTED

--- src/controllers/Connect4Controller.py
+++ src/controllers/Connect4Controller.py
@@ -1,10 +1,55 @@
 #/src/controllers/TodoController.py
+from flask import Flask, make_response, current_app
+from datetime import timedelta
+from functools import update_wrapper
 from flask import request, g, Blueprint, json, Response
 from ..models.Connect4Model import connect4Predict
 import requests
 import threading
 
 connect4_api = Blueprint('connect4_api', __name__)
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+              max_age=21600, attach_to_all=True,
+              automatic_options=True):
+  if methods is not None:
+      methods = ', '.join(sorted(x.upper() for x in methods))
+  if headers is not None and not isinstance(headers, str):
+      headers = ', '.join(x.upper() for x in headers)
+  if not isinstance(origin, str):
+      origin = ', '.join(origin)
+  if isinstance(max_age, timedelta):
+      max_age = max_age.total_seconds()
+
+  def get_methods():
+      if methods is not None:
+          return methods
+
+      options_resp = current_app.make_default_options_response()
+      return options_resp.headers['allow']
+
+  def decorator(f):
+      def wrapped_function(*args, **kwargs):
+          if automatic_options and request.method == 'OPTIONS':
+              resp = current_app.make_default_options_response()
+          else:
+              resp = make_response(f(*args, **kwargs))
+          if not attach_to_all and request.method != 'OPTIONS':
+              return resp
+
+          h = resp.headers
+
+          h['Access-Control-Allow-Origin'] = origin
+          h['Access-Control-Allow-Methods'] = get_methods()
+          h['Access-Control-Max-Age'] = str(max_age)
+          if headers is not None:
+              h['Access-Control-Allow-Headers'] = headers
+          return resp
+
+      f.provide_automatic_options = False
+      return update_wrapper(wrapped_function, f)
+  return decorator
 
 
 @connect4_api.route('/', methods=['GET'])
@@ -19,6 +64,7 @@ def get_all():
   return custom_response(connect4, 200)
 
 @connect4_api.route('/predict', methods=['POST'])
+@crossdomain(origin="*")
 def do_predict():
   """
   Get All Connect4
@@ -27,7 +73,7 @@ def do_predict():
   req_data = request.get_json()
   
   if req_data.get('state') and req_data.get('available_actions'):
-    prediction = connect4Predict(req_data.get('state'), req_data.get('available_actions'))
+    prediction = connect4Predict(req_data.get('state'), req_data.get('available_actions'), req_data.get('steps_done'))
     print("====================================")
     print(prediction)
     print("====================================")
